a13ce618eb972bb76d85f06fa4a02710d199c153
Filippov Konstantin <fkv.katafey@yandex.ru>
2024-08-29 23:55:30 +0400

    Создал перечисление статических констант. Добавил viewController, на него добавил UIImageView и UIButton, установил логотип а также соответствующие размеры и шрифты из макета согласно тех заданию.

---
+++ ImageFeed/Constants.swift
@@ -0,0 +1,11 @@
+import UIKit
+
+enum Constants {
+    static let accessKey = "FjboiHJifnIgeMzjX4GftScq06L9uM6ENVsXGBGHgGc"
+    static let secretKey = "BeIfnH5J6JfabXKs_oVz1W8dMIkl4cKC4C3_ljTBbus"
+    static let redirectURI = "urn:ietf:wg:oauth:2.0:oob"
+    static let accessScope = "public+read_user+write_likes"
+    static let defaultBaseURL = URL(string: "https://api.unsplash.com/")!
+}
+
+
