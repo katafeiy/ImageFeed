11a9d163a6ca1c8b6e1771874e8e3414dee03ef8
Filippov Konstantin <fkv.katafey@yandex.ru>
2024-10-16 01:59:44 +0400

    Корректировка кода согласно замечаниям ревьювера

--- ImageFeed/Profile/ProfileModels.swift
+++ ImageFeed/Profile/ProfileModels.swift
@@ -23,20 +23,3 @@ struct Profile {
     let bio: String?
 }
 
-// MARK: Модель для декодирования данных аватарки
-
-struct UserResult: Codable {
-    let profileImage: ImageURL?
-    
-    enum CodingKeys: String, CodingKey {
-        case profileImage = "profile_image"
-    }
-}
-
-struct ImageURL: Codable {
-    let small: String?
-    
-    enum CodingKeys: String, CodingKey {
-        case small = "small"
-    }
-}
